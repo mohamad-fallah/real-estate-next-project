d0c815b890782eb977bb35e0394764faf52cd036
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-09-17 10:02:38 +0330

    structure project

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Features from '../components/template/Index/Features'
+import StoryPictures from '@/components/template/Index/StoryPictures'
+import Homes from '@/components/template/Index/Homes'
+import Galery from '@/components/template/Index/Galery'
 
 function index() {
   return (
-    <h1>Home Page</h1>
+    <>
+    <Features/>
+    <StoryPictures/>
+    <Homes/>
+    <Galery/>
+    </>
   )
 }
 
